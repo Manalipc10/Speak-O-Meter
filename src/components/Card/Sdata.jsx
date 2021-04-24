d8082d62236ca2e7bd2d70da448e1729dfed66fc
Manali <manalipc10@gmail.com>
2021-04-24 23:02:26 +0530

    week 1

---
+++ src/components/Card/Sdata.jsx
@@ -0,0 +1,41 @@
+import story1 from "./sample.jpg";
+import story2 from "./sample.jpg";
+import story3 from "./sample.jpg";
+import story4 from "./sample.jpg";
+import story5 from "./sample.jpg";
+import story6 from "./sample.jpg";
+
+const Sdata=[
+    {
+        imgsrc:story1,
+        title:"Story 1",
+    },
+
+    {
+        imgsrc:story2,
+        title:"Story 2",
+    },
+
+    {
+        imgsrc:story3,
+        title:"Story 3",
+    },
+
+    {
+        imgsrc:story4,
+        title:"Story 4",
+    },
+
+    {
+        imgsrc:story5,
+        title:"Story 5",
+    },
+
+    {
+        imgsrc:story6,
+        title:"Story 6",
+    },
+
+];
+
+export default Sdata;
